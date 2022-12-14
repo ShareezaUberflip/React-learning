81d0611847bf7d6619204f253793a53e9f5b2287
Shareeza Hussain <78374046+ShareezaUberflip@users.noreply.github.com>
2022-12-14 13:18:55 -0500

    code cleanup

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import data from './data';
+//import data from './data';
 import List from './List';
 import axios from 'axios';
 
@@ -7,7 +7,7 @@ import axios from 'axios';
 
 
 function App() {
-  const [people, setPeople] = useState(data);
+  const [people, setPeople] = useState([]);
 
   const getPerson = async() => {
     const response = await axios.get('https://jsonplaceholder.typicode.com/users');
