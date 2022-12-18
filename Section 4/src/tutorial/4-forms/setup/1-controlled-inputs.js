82570e2c99c624c3ee0d0480a1af2d105fcd84a9
Shareeza <your>
2022-12-18 17:12:16 -0500

    practise form

--- Section 4/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ Section 4/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,7 +6,55 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const [person, setPerson] = useState({ firstName: '', email: '', age: '' })
+  const [people, setPeople] = useState([]);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (person.firstName && person.email && person.age) {
+      const newPerson = { ...person, id: new Date().getTime().toString() };
+      setPeople([...people, newPerson]);
+      setPerson({ firstName: '', email: '', age: '' });
+    }
+  };
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setPerson({ ...person, [name]: value });
+  };
+
+  return <>
+
+    <section>
+      <form className='form'>
+        <div className='form-control'>
+          <label htmlFor='first-name'> First Name: </label>
+          <input type='text' value={person.firstName} name='firstName' id='first-name' onChange={handleChange} />
+        </div>
+        <div className='form-control'>
+          <label htmlFor='email'> Email: </label>
+          <input type='email' value={person.email} name='email' id='email' onChange={handleChange} />
+        </div>
+        <div className='form-control'>
+          <label htmlFor='age'> Age: </label>
+          <input type='number' value={person.age} name='age' id='age' onChange={handleChange} />
+        </div>
+        <button type='submit' onClick={handleSubmit}>Add person</button>
+      </form>
+      {people.map((person, index) => {
+        return (
+          <div className='item' key={index}>
+            <p>{person.firstName}</p>
+            <p>{person.age}</p>
+            <p>{person.email}</p>
+          </div>
+        );
+      })
+      }
+    </section>
+  </>;
 };
 
 export default ControlledInputs;
